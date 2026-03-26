643ee18415e8570e340e0f6b565127d1fc563aef
KaykyOne <kaykyzioti@gmail.com>
2026-03-26 10:07:17 -0300

    add seeder and factory

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\invoice;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends Factory<invoice>
@@ -17,8 +18,13 @@ class InvoiceFactory extends Factory
      */
     public function definition(): array
     {
+        $paid = $this->faker->boolean();
         return [
-            //
+            'user_id' => User::all()->random()->id,
+            'type' => $this->faker->randomElement(['P', 'B', 'C']),
+            'paid' => $paid,
+            'value' => $this->faker->numberBetween(1000, 10000),
+            'payment_date' => $paid ? $this->faker->randomElement([$this->faker->dateTimeThisMonth()]) : NULL,
         ];
     }
 }
